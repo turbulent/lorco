4a4d61eb5939773e3b9ee2ad95bda4bf3726e19f
Anthony Guimard <aguimard@turbulent.ca>
2019-01-24 15:01:52 -0500

    Add dummy tests for languages build target.

--- index.js
+++ index.js
@@ -10,24 +10,32 @@ const createFile = require('./helpers/createFile');
 const file = `${__dirname}/files/${getFile()}`
 const language = getLanguage();
 
-ns.read(file)
-  .then(sketch => {
-    const { symbols } = sketch;
-    const fileName = path.basename(file, '.sketch');
+const lorco = (file, language) => {
+  return ns.read(file)
+    .then(sketch => {
+      const { symbols } = sketch;
+      const fileName = path.basename(file, '.sketch');
 
-    const colors = symbols.map((symbol) => {
-      const [layer, ...others] = symbol.layers;
-      const style = layer.get('style').toJson();
-      const [fill] = style.fills;
+      const colors = symbols.map((symbol) => {
+        const [layer, ...others] = symbol.layers;
+        const style = layer.get('style').toJson();
+        const [fill] = style.fills;
 
-      const { color } = fill;
+        const { color } = fill;
 
-      const rgbacolor = transformToRGBA(color);
-      const name = symbol.name;
+        const rgbacolor = transformToRGBA(color);
+        const name = symbol.name;
 
-      return createVariable(name, rgbacolor, language);
-    });
+        return createVariable(name, rgbacolor, language);
+      });
 
-    createFile(`_${fileName}`, colors, language);
-  })
-  .catch((err) => new Error('Error: ', err));
+      createFile(`_${fileName}`, colors, language);
+
+      return colors;
+    })
+    .catch((err) => new Error('Error: ', err));
+}
+
+lorco(file, language);
+
+module.exports = lorco;
